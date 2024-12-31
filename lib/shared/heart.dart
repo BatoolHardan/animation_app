799cf4330651d74040377ef9b1a01d641fcc23c1
Batool Hardan <batool.hardan2001@gmail.com>
2024-12-31 13:53:31 +0200

    Enlarge and shrink the heart  by TweenSequence

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -11,16 +11,24 @@ class Heart extends StatefulWidget {
 class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
   bool isFav = false;
   late AnimationController controller;
-  late Animation colorAnimation;
+  late Animation<Color?> colorAnimation;
+  late Animation<double> sizeAnimation;
   @override
   void initState() {
     super.initState();
 
     controller =
-        AnimationController(duration: Duration(microseconds: 500), vsync: this);
+        AnimationController(duration: Duration(milliseconds: 300), vsync: this);
 
     colorAnimation = ColorTween(begin: Colors.grey[400], end: Colors.red)
         .animate(controller);
+
+    sizeAnimation = TweenSequence(<TweenSequenceItem<double>>[
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 30, end: 50), weight: 50),
+      TweenSequenceItem<double>(
+          tween: Tween<double>(begin: 50, end: 30), weight: 50),
+    ]).animate(controller);
 
     controller.addListener(() {
       print(controller.value);
@@ -58,7 +66,7 @@ class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
           icon: Icon(
             Icons.favorite,
             color: colorAnimation.value,
-            size: 30,
+            size: sizeAnimation.value,
           ),
           onPressed: () {
             isFav ? controller.reverse() : controller.forward();
